ed337d53d1f2450b2e4271c75071fd554062b104
Rafa <rafael@avena.io>
2025-09-09 22:48:25 -0600

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 // src/index.ts
 import "./styles/tokens.css"; // si quieres exportar estilos globales
 export { Button } from "./components/Button/Button";
-export { Input } from "./components/Input/Input";
-export { Card } from "./components/Card/Card";
 export type { ButtonProps, ButtonVariant, ButtonSize } from "./components/Button/Button";
-export type { CardProps } from "./components/Card/Card";
+
